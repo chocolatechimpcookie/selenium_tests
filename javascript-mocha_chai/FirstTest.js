a972648118bd4d5e391d718b831b8ca980d7fdf8
cina <memaralicina@gmail.com>
2019-12-11 00:11:21 -0500

    got rid of unnecessary folder

--- javascript-mocha_chai/FirstTest.js
+++ javascript-mocha_chai/FirstTest.js
@@ -4,16 +4,21 @@ const {By, until} = webdriver;
 const chai = require('chai');
 const chaiAsPromised = require('chai-as-promised')
 chai.use(chaiAsPromised);
-const driver = new webdriver.Builder().forBrowser('chrome').build();
+const driver = new webdriver.Builder().forBrowser('firefox').build();
 const expect = chai.expect;
 
 describe
-('Instagram Test', done => {
+('Instagram Test', function() {
     driver.get('https://www.instagram.com/');
-    driver.sleep(3000);
-    var login_button = driver.findElement(By.xpath("//a[@href = '/accounts/login/?source=auth_switcher']"));
+    driver.sleep(20000);
+
+    it('login', function()
+    {
+      var login_button = driver.findElement(By.xpath("//a[@href = '/accounts/login/?source=auth_switcher']"));
+
+    });
     // why isnt this working? EIther I typed it wrong or maybe some kind of asychronity
-    // login_button.click();
+    //login_button.click();
 
 
 
